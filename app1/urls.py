06050ac2747714349255d52a83d72b7e6f578bec
naseef434 <naseefp.hin@gmail.com>
2020-10-25 03:27:49 +0530

    dashboard layout completed

--- app1/urls.py
+++ app1/urls.py
@@ -3,6 +3,8 @@ from . import views
 urlpatterns = [
    
     path('', views.index),
+    path('login', views.login),
     path('register', views.register),
-
+    
+  
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+def login(request):
     return render(request, 'login.html')
 
 def register(request):
     return render(request, 'register.html')
+
+def index(request):
+    return render(request, 'test.html') 
+
+
+def Adminarea(parameter_list):
+    """
+    docstring
+    """
+    pass
